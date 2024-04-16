a066c4b74d572b40d20db090a83b4259571de1ed
Samy <cherif.samy1000@gmail.com>
2024-04-16 23:34:52 +0100

    Added registering

--- src/main/java/com/example/realworld/RealworldApplication.java
+++ src/main/java/com/example/realworld/RealworldApplication.java
@@ -1,9 +1,10 @@
-package com.example.Realworld;
+package com.example.realworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class RealworldApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/realworld/RealworldApplicationTests.java
+++ src/test/java/com/example/realworld/RealworldApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Realworld;
+package com.example.realworld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
